e3ce2d1b550580be613731cd329a91a940d4aa10
Leart Bytyqi <leart.bytyqii98@gmail.com>
2025-06-21 16:52:58 +0200

    Fix start.sh permissions and artisan setup

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# Jep leje të duhura për storage dhe bootstrap/cache
+cd /var/www
+
+# Fix permissions
 chmod -R 775 storage bootstrap/cache
 chown -R www-data:www-data storage bootstrap/cache
 
-# Ruaj konfigurimin e Laravel
+# Laravel setup
+php artisan config:clear
+php artisan migrate --force
 php artisan config:cache
 
-# Run migrimet automatikisht në production
-php artisan migrate --force
-
-# Starto serverin e Laravel në portin që Railway pret
-php artisan serve --host=0.0.0.0 --port=8000
+# Start NGINX dhe PHP-FPM
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
